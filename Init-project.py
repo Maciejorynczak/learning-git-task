f2fcf46ce66a030fd2d5b69e8478590932dfa966
Maciej Oryńczak <maciejorynczak@gmail.com>
2024-05-08 16:35:53 +0200

    Add shopping_dict

---
+++ Init-project.py
@@ -0,0 +1,6 @@
+shopping_list = ["marchew", "seler", "rukola","chleb", "bułki", "pączek"]
+
+shopping_dict = {
+    "warzywniak": ["marchew", "seler", "rukola"],
+    "piekarnia": ["chleb", "bułki", "pączek" ]
+} 
