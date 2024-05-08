988c4009243e8d35373675a7c04e89755c195baf
Maciej Oryńczak <maciejorynczak@gmail.com>
2024-05-08 16:46:24 +0200

    Add product_sum

--- Init-project.py
+++ Init-project.py
@@ -4,4 +4,8 @@ shopping_dict = {
     "warzywniak": ["marchew", "seler", "rukola"],
     "piekarnia": ["chleb", "bułki", "pączek" ]
 } 
-
+for shop in shopping_dict:
+    product = [product.capitalize() for product in shopping_dict[shop]]
+    print(f"Idę do {shop.capitalize()} ,i kupuję tu następujące rzeczy: {product}.")
+product_sum = sum(len(products) for products in shopping_dict.values())
+print(f"W sumie kupuję {product_sum} produktów. ")
