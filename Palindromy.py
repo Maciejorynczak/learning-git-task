43b888578f130edbdacca7e41b70e47e050c7cc6
Maciej Oryńczak <maciejorynczak@gmail.com>
2024-06-11 14:58:37 +0200

    zmniejszenie liter oraz usunięcie spacji

--- Palindromy.py
+++ Palindromy.py
@@ -1,4 +1,6 @@
 def palindrom(slowo):
+    slowo = slowo.replace(" ","").lower()
     return slowo == slowo[::-1]
 print(palindrom("kajak"))
 print(palindrom("okno"))
+print(palindrom("Kobyla ma maly bok"))
