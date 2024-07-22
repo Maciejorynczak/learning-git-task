2d73f131b756171f08b7cb681eb9979e92669325
Maciej Oryńczak <maciejorynczak@gmail.com>
2024-07-22 12:36:00 +0200

    poprawki i sprawdzenie

--- Palindromy.py
+++ Palindromy.py
@@ -1,6 +1,15 @@
+import string
+import unicodedata
+
 def palindrom(slowo):
-    slowo = slowo.replace(" ","").lower()
+# Normalizowanie tekstu, usuwanie polskich znaków
+    slowo = unicodedata.normalize('NFD', slowo)
+    slowo = ''.join(c for c in slowo if unicodedata.category(c) != 'Mn')
+# Usuwanie wszystkich znaków interpunkcyjnych i spacji
+    slowo = slowo.translate(str.maketrans('', '', string.punctuation)).replace(" ","").lower()
     return slowo == slowo[::-1]
+
 print(palindrom("kajak"))
 print(palindrom("okno"))
 print(palindrom("Kobyla ma maly bok"))
+print(palindrom("Zula łowi, tenora bada baronet i woła - luz!"))
